4a55bdf2cac847032a28cf4138f03a7121fd4e23
Yoo JinYoung <young@acadeem.io>
2025-03-17 15:19:42 +0900

    2025-03-17 15:15

--- 07-RDBMS_amazon/src/modules/orders.module.js
+++ 07-RDBMS_amazon/src/modules/orders.module.js
@@ -12,41 +12,66 @@ ordersRouter.post("/", async (req, res, next) => {
     const token = req.headers.authorization.split("Basic ")[1];
     const userId = token.slice(1, -1);
 
-    const order = await prisma.order.create({
-      data: {
-        userId,
-      },
-    });
-
-    let totalAmount = 0;
-    const promises = data.map(async ({ productId, quantity }) => {
-      const { price } = await prisma.product.findUnique({
-        where: { id: productId },
-        select: { price: true },
-      });
-      const amount = price * quantity;
-      totalAmount += amount;
-
-      await prisma.orderItem.create({
+    await prisma.$transaction(async (tx) => {
+      const order = await tx.order.create({
         data: {
-          orderId: order.id,
-          productId,
-          quantity,
-          amount,
+          userId,
         },
       });
+
+      let totalAmount = 0;
+      const promises = data.map(async ({ productId, quantity }) => {
+        const { price } = await tx.product.findUnique({
+          where: { id: productId },
+          select: { price: true },
+        });
+        const amount = price * quantity;
+        totalAmount += amount;
+
+        await tx.orderItem.create({
+          data: {
+            orderId: order.id,
+            productId,
+            quantity,
+            amount,
+          },
+        });
+      });
+
+      await Promise.all(promises);
+
+      const updatedOrder = await tx.order.update({
+        where: { id: order.id },
+        data: { totalAmount },
+      });
+
+      res.json(updatedOrder);
     });
+  } catch (e) {
+    next(e);
+  }
+});
 
-    await Promise.all(promises);
+ordersRouter.post("/payment", async (req, res, next) => {
+  try {
+    const data = req.body;
+    const { orderId, paidAmount } = data;
 
-    console.log(totalAmount);
+    await prisma.$transaction(async (tx) => {
+      let updatedOrder = await tx.order.update({
+        where: { id: orderId },
+        data: { paidAmount: { increment: paidAmount } },
+      });
 
-    const updatedOrder = await prisma.order.update({
-      where: { id: order.id },
-      data: { totalAmount },
+      if (updatedOrder.totalAmount === updatedOrder.paidAmount) {
+        updatedOrder = await tx.order.update({
+          where: { id: updatedOrder.id },
+          data: { status: "paid" },
+        });
+      }
+
+      res.json(updatedOrder);
     });
-
-    res.json(updatedOrder);
   } catch (e) {
     next(e);
   }
