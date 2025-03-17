bfc9c727a4a0d2433540b99222ee7480154b9745
Yoo JinYoung <young@acadeem.io>
2025-03-17 16:50:02 +0900

    2025-03-17 16:50

--- 07-RDBMS_amazon/src/app.js
+++ 07-RDBMS_amazon/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = require("./modules/index.module");
 const errorHandler = require("./middlewares/errorHandler.middleware");
+const authentication = require("./middlewares/authentication.middleware");
 
 const app = express();
 const PORT = 5555;
 
+app.use(authentication);
 app.use(express.json());
 app.use(router);
 app.use(errorHandler);
