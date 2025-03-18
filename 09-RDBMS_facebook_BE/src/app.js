d7a0c8bd7fc003572b9855cb8cd04f9810210cdb
Yoo JinYoung <young@acadeem.io>
2025-03-18 10:53:17 +0900

    2025-03-18 11:00

---
+++ 09-RDBMS_facebook_BE/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = require("./modules/index.module");
+const errorHandler = require("./middlewares/errorHandler.middleware");
+
+const app = express();
+const PORT = 7777;
+
+app.use(express.json());
+app.use(router);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server started to listen at port number ${PORT}...`);
+});
