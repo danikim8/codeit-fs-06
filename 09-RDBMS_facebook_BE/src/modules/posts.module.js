73336ca7a47119217cbdedf41837821d2572b8c9
Yoo JinYoung <young@acadeem.io>
2025-03-18 11:49:46 +0900

    2025-03-18 11:50

--- 09-RDBMS_facebook_BE/src/modules/posts.module.js
+++ 09-RDBMS_facebook_BE/src/modules/posts.module.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const prisma = require("../db/prisma/client.prisma");
 const userOnly = require("../middlewares/userOnly.middleware");
+const Exception = require("../exceptions");
 
 const postsRouter = express.Router();
 
@@ -17,6 +18,40 @@ postsRouter.post("/", userOnly, async (req, res, next) => {
     });
 
     res.json(post);
+  } catch (e) {
+    next(e);
+  }
+});
+
+/**
+ * 글 조회하기
+ */
+postsRouter.get("/:postId", async (req, res, next) => {
+  try {
+    const postId = Number(req.params.postId);
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      include: { comments: true },
+    });
+    if (!post) throw new Exception(404, "존재하지 않는 게시글입니다");
+
+    res.json(post);
+  } catch (e) {
+    next(e);
+  }
+});
+
+/**
+ * 글 좋아요 누르기
+ */
+postsRouter.put("/:postId/like", userOnly, async (req, res, next) => {
+  try {
+    const postId = Number(req.params.postId);
+    const userId = req.userId;
+
+    const like = await prisma.like.create({ data: { userId, postId } });
+
+    res.json(like);
   } catch (e) {
     next(e);
   }
